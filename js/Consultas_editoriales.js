abeca00dc2514a10242c4af6577a6952fee76b85
rinwer <rinsonjawer@gmail.com>
2022-09-30 11:47:11 -0500

    Metodo consultar editorial ok

--- js/Consultas_editoriales.js
+++ js/Consultas_editoriales.js
@@ -11,33 +11,44 @@ bottom.addEventListener("click", hizoClick);
 editoriales = [];
 let bottomBuscar = document.getElementById("bottomBuscarEditorial");
 
-const geteditorialUrl =
-  "https://minticgrupo4.herokuapp.com/libros/consultar_editoriales";
+//const getlibroUrl = "https://minticgrupo4.herokuapp.com/libros/consultarLibroNombre/";
+let getEditorialUrl = "http://127.0.0.1:8000/libros/consultarEditorial/";
 
 function clickBuscarEditorial() {
-  geteditoriales();
+  console.log("cuando se hace click");
+  getEditorial();
 }
-function geteditoriales() {
-  fetch(getlibrosUrl)
+
+function getEditorial() {
+  //Capturar la url
+  //const url = new URL(window.location.href);
+  //const id = url.searchParams.get("cod_libro");
+  console.log("Antes de capturar el value");
+  const editorial = document.formCOnsulta.des_editorial.value;
+  console.log("Editorial value: " + editorial);
+  fetch(getEditorialUrl + editorial)
     .then((response) => {
-      console.log(response.status);
-      if (response.ok) {
+      if (response.ok || response.status == 400) {
         return response.text();
       } else {
         throw new Error(response.status);
       }
     })
+    //recibimos un json en data
     .then((data) => {
+      if (data.includes("No existe libro")) {
+        funcionError(data);
+      }
       //convertir objeto del back en objecto json.
       editoriales = JSON.parse(data);
-      procesareditoriales();
+      procesarEditorial();
     })
     .catch((err) => {
-      console.error("ERROR: ", err.message);
+      console.log("Catch: " + err.message);
     });
 }
-
-function procesareditoriales() {
+function procesarEditorial() {
+  document.getElementById("main").innerHTML = "";
   const tabla = document.createElement("table");
   const hileraHeader = document.createElement("tr");
 
@@ -48,7 +59,7 @@ function procesareditoriales() {
     hileraHeader.appendChild(celdaHeder);
   }
   tabla.appendChild(hileraHeader);
-  for (let i = 0; i < libros.length; i++) {
+  for (let i = 0; i < editoriales.length; i++) {
     const hilera = document.createElement("tr");
     for (let j in editoriales[i]) {
       const celda = document.createElement("td");
@@ -58,9 +69,24 @@ function procesareditoriales() {
     }
     tabla.appendChild(hilera);
   }
-  document.getElementById("contenido").remove();
+  if (document.getElementById("contenido") != null) {
+    document.getElementById("contenido").remove();
+  }
+  if (editoriales.length == 0) {
+    document.getElementById("main").innerHTML = `
+    <h2>No existe coincidencia. Intente buscar de nuevo</h2>
+    `;
+  }
   const info = document.getElementById("main");
   info.appendChild(tabla);
+}
+
+function funcionError(err) {
+  if (err) {
+    document.getElementById("main").innerHTML = `
+    <h2>${err}</h2>
+    `;
+  }
 }
 
 bottomBuscar.addEventListener("click", clickBuscarEditorial);
